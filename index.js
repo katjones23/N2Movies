df8384ec607215d4680af818cfb79cf4fdbad692
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-04-07 22:08:39 -0400

    added a little style to the page

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+ 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('input-field');
+    var instances = M.FormSelect.init(elems, options);
+  });
+  
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
