09d94d914f068817ea19b4ce22e21c502e0be6d0
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-04-08 18:28:18 -0400

    still working on the search bar

--- index.js
+++ index.js
@@ -3,16 +3,13 @@
     $('.sidenav').sidenav();
   });
  
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-  });
+ 
   
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('input-field');
     var instances = M.FormSelect.init(elems, options);
   });
-  
+
   $(document).ready(function(){
     $('select').formSelect();
   });
