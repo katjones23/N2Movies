f81373dfbb657ab70fef0ab593ee882ab4be3075
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-04-08 20:38:24 -0400

    started over so we wont have a bulky branch

--- index.js
+++ index.js
@@ -5,11 +5,4 @@
  
  
   
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('input-field');
-    var instances = M.FormSelect.init(elems, options);
-  });
-
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
+ 
