99dc7c42b831b7ca2194b6adba2c1d6eae64a2e2
Katherine Jones <kejone1414@gmail.com>
2020-04-08 22:02:06 -0400

    feat: got the basics of the DDD api working

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
- 
- 
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('input-field');
-    var instances = M.FormSelect.init(elems, options);
-  });
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
 
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('input-field');
+  var instances = M.FormSelect.init(elems, options);
+});
+
+$(document).ready(function () {
+  $('select').formSelect();
+});
+
+//DtDD API
+var dtddAPIKey = "fa2a38047e3e593c5387a8ba5da60da0"
+var dtddUrl = "https://cors-anywhere.herokuapp.com/https://www.doesthedogdie.com/dddsearch?q="
+var dtddUrlMedia = "https://cors-anywhere.herokuapp.com/https://www.doesthedogdie.com/media/"
+
+//Get the DDD ID
+$.ajax({
+  // test string for search, update once var available
+  url: dtddUrl + "old%20yeller",
+  method: "GET",
+  headers: {
+    "Accept": "application/json",
+    "X-API-Key": dtddAPIKey,
+  },
+})
+  .then(function (response) {
+    var dddID = response.items[0].id;
+
+    //Get topic
+    $.ajax({
+      url: dtddUrlMedia + dddID,
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "X-API-Key": dtddAPIKey,
+      },
+    })
+      .then(function (response) {
+        var topicItems = response.topicItemStats
+        // for each item, see if yes > no
+        topicItems.forEach(function(item, index) {
+          if (item.yesSum >= item.noSum) {
+            var topicName = topicItems[index].topic.name
+            console.log("Users report " + topicName)
+          }
+        });
+      })
+      .catch(function (error) {
+        //push "no results found to page"
+      })
+  })
+  .catch(function (response) {
+    //push "no results found to page"
+  })
+
+
+
