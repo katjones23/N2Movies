51c7d3719252053809012502617cfaa8c967eb2e
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-04-13 21:31:40 -0400

    feat:

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 
   $(document).ready(function(){
     $('.sidenav').sidenav();
+    alert('hi');
+   
   });
- 
- 
+
+  $(".searchBtn").on("click", function(event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
+    alert('hi');
+
+    // This line will grab the text from the input box
+    var movie = $("#movie-input").val().trim();
+    // The movie from the textbox is then added to our array
+    movies.push(movie);
+
+    // calling renderButtons which handles the processing of our movie array
+    renderButtons();
+  });
+
+
   
  
