f7bb34f6a4bd9a0ac9219acb33466068610139a8
Katherine Jones <kejone1414@gmail.com>
2020-04-14 18:39:10 -0400

    feat: added storage and render functions for saving five most recent searches

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
- 
- 
-  
- 
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+
+//  Recent search storage; put save inside event listener for search button
+var searchHistoryBlock = $("<div>").addClass("searchHistoryBlock")
+var prevSearches = [];
+
+function init() {
+  var storedSearches = JSON.parse(localStorage.getItem("prevSearchesStore"));
+
+  if (storedSearches !== null) {
+    prevSearches = storedSearches;
+    renderPrevSearches();
+  } else {
+    return;
+  };
+};
+
+init();
+
+function renderPrevSearches() {
+  $(searchHistoryBlock).empty();
+
+  if (prevSearches.length > 5) {
+    prevSearches.splice(0, 1);
+  }
+
+  for (var i = 0; i < prevSearches.length; i++) {
+    var prevSearch = prevSearches[i];
+    var div = $("<div>")
+
+    $(searchHistoryBlock).css("display", "block")
+    $(div).text(prevSearch);
+    $(div).addClass("searchHistoryItem");
+    $(searchHistoryBlock).prepend(div);
+  }
+}
+
+function localSaveSearch() {
+  var div = $("<div>")
+  var inputText = ("#zInput").val();
+
+  $(searchHistoryBlock).css("display", "block")
+  $(div).text(inputText);
+  $(div).addClass("searchHistoryItem");
+  $(searchHistoryBlock).prepend(div);
+
+  prevSearches.push(inputText);
+  localStorage.setItem("prevSearchesStore", JSON.stringify(prevSearches));
+
+};
+
+localSaveSearch();
