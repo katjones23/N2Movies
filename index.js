85693716466cea7da29e04f04404fb6336eca297
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-04-16 19:19:19 -0400

    feat:will come back to this in a min

--- index.js
+++ index.js
@@ -125,6 +125,10 @@ $(document).ready(function () {
   $(document).ready(function(){
     $('.modal').modal();
   });
+  
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
  
  
   
